5b972cecb59f617d8469fcc2125dbc836d157e99
TauriR <116064302+TauriRu@users.noreply.github.com>
2022-12-05 17:46:01 +0200

    styled section, layout and button

---
+++ src/App.styles.ts
@@ -0,0 +1,76 @@
+import styled, { createGlobalStyle } from "styled-components";
+import BGImage from "./images/rafael-garcin-gDSQLdCSfoE-unsplash.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    height: 100%
+}
+
+body {
+    background-image: url(${BGImage});
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+    height: 100%;
+    align-items:center;
+}
+
+* {
+    box-sizing:border-box;
+    font-family: 'Ubuntu';
+}
+
+
+
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f3f3f3;
+  border-radius: 15px;
+  padding:20px;
+  place-content:center;
+  box-shadow: 0 5px 10px $33333;
+
+  > p {
+    color: #333333
+  }
+
+  .score{
+    color: #fff
+    font-size: 1.5rem;
+    margin: 0;
+  }
+  h1{
+    font-family: 'ubuntu';
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    background-size: 100%;
+    background-clip: text;
+    -webkit-background-clip:text;
+    -webkit-text-fill-color:transparent;
+    -moz-background-clip:text;
+    -moz-text-fill-color: transparent;
+    filter:drop-shadow(2px 2px #0085a3);
+    font-size: 50px;
+    font-weight: 400;
+    text-align:center;
+    margin:20px;
+  }
+  .start, .next{
+    cursor: pointer;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    border: none;
+    box-shadow: 0 5px 15px #0085a3;
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 25px;
+  }
+  .start {
+    max-width: 200px;
+  }
+`;
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+// Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles'
 
 type Props = {
     question: string;
@@ -10,19 +12,22 @@ type Props = {
 }
 
 const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNumber, totalQuestions }) => (
-    <div>
+    <Wrapper>
         <p className="number">Question: {questionNumber} / {totalQuestions} </p>
         <p dangerouslySetInnerHTML={{ __html: question }} />
         <div>
             {answers.map(answer => (
-                <div key={answer}>
+                <ButtonWrapper key={answer}
+                    correct={userAnswer?.correctAnswer === answer}
+                    userClicked={userAnswer?.answer === answer}
+                >
                     <button disabled={userAnswer} value={answer} onClick={callback}>
                         <span dangerouslySetInnerHTML={{ __html: answer }} />
                     </button>
-                </div>
+                </ButtonWrapper>
             ))}
         </div>
-    </div>
+    </Wrapper>
 );
 
 export default QuestionCard;
